adf2d25237f7275b1c86ee97bfce765d2f0bca43
tanialuo <75762582+tanialuo@users.noreply.github.com>
2023-12-17 21:40:02 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".offscreen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+console.log("test");
